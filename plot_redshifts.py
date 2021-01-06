6b79e360c962c1b5ac7b20dd186f459494cab1fc
Liam Dubay <liam.dubay@gmail.com>
2021-01-05 19:28:51 -0800

    added subtle grid overlay

--- plot_redshifts.py
+++ plot_redshifts.py
@@ -3,19 +3,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 from pathlib import Path
 
-bin_width = 0.025
+# Settings
+bin_width = 0.02
+z_min = 0.
+z_max = 0.5
 
 sn_info = pd.read_csv(Path('ref/sn_info.csv'), index_col='name')
 z = sn_info['z']
 z = z[pd.notna(z)].astype(float)
-bins = int((max(z) - min(z)) / bin_width)
+bins = int((z_max - z_min) / bin_width)
 
 fig, ax = plt.subplots()
-plt.hist(z, bins=bins, histtype='bar', color='#e67', rwidth=0.95)
+plt.hist(z, bins=bins, range=(z_min, z_max), histtype='bar', color='#e67', rwidth=1.)
 fig.set_tight_layout(True)
 plt.xlabel('Redshift')
 plt.ylabel('Number of SNe Ia')
-plt.xlim((0,0.5))
+plt.xlim((z_min, z_max))
+
+plt.grid(b=True, which='major', axis='x', color='w', lw=2)
+plt.grid(b=True, which='minor', axis='x', color='w', lw=1)
+plt.grid(b=True, which='major', axis='y', color='w', lw=1)
 
 plt.savefig(Path('out/redshifts.pdf'), bbox_inches='tight', dpi=300)
 plt.show()
