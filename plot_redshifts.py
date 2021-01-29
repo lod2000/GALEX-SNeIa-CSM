affc27749faaee0bfc335f66fc900e6bbd4d5388
Liam Dubay <liam.dubay@gmail.com>
2021-01-28 17:12:00 -0800

    replaced plot frame

--- plot_redshifts.py
+++ plot_redshifts.py
@@ -22,33 +22,15 @@ fig, ax = plt.subplots()
 
 hist = ax.hist(z, bins=bins, range=(z_min, z_max), histtype='bar', color=color)
 
+ax.grid(b=True, which='major', axis='x', color='w', lw=1)
+ax.grid(b=True, which='minor', axis='x', color='w', lw=1)
+ax.grid(b=True, which='major', axis='y', color='w', lw=1)
+
 ax.set_xlabel('Redshift')
 ax.set_ylabel('Number of SNe Ia')
-
-plt.grid(b=True, which='major', axis='x', color='w', lw=2)
-plt.grid(b=True, which='minor', axis='x', color='w', lw=1)
-plt.grid(b=True, which='major', axis='y', color='w', lw=1)
-
-# Adjust ticks and frame
-ax.set_xlim((z_min - bin_width, z_max))
-ax.set_ylim((-minor_tick_space, None))
-
-hist_arr = np.histogram(z, bins=bins, range=(z_min, z_max))
-yticks_major = np.arange(0, np.max(hist_arr[0]) + major_tick_space, major_tick_space)
-ax.yaxis.set_major_locator(ticker.FixedLocator(yticks_major))
-yticks_minor = np.arange(0, np.max(hist_arr[0]) + minor_tick_space, minor_tick_space)
-ax.yaxis.set_minor_locator(ticker.FixedLocator(yticks_minor))
-ax.tick_params(axis='both', which='both', top=False, right=False)
-
-ax.spines['right'].set_visible(False)
-ax.spines['top'].set_visible(False)
-ax.spines['bottom'].set_bounds(z_min, z_max)
-ax.spines['left'].set_bounds(0, yticks_minor[-1])
-
-ax.set_ylabel('Number of SNe Ia', rotation='horizontal', ha='left', y=1.05, labelpad=-1)
+ax.set_ylim((None, 110))
 
 plt.tight_layout(pad=0.3)
-plt.subplots_adjust(top=0.88)
 
 plt.savefig(Path('out/redshifts.pdf'), bbox_inches='tight', dpi=300)
 
