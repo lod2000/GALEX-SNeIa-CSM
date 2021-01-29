c7ef17aa6e15cad848c4a2cbdaf3a29a495e394d
Liam Dubay <liam.dubay@gmail.com>
2021-01-28 16:40:00 -0800

    replaced plot frame

--- plot_filters.py
+++ plot_filters.py
@@ -4,21 +4,10 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 from pathlib import Path
 
-# Plot settings
-x_min = 1300
-x_max = 3300
-y_min = 0
-dx_major = 500 # tick spacing
-dx_minor = 100
-dy_major = 4
-dy_minor = 1
-
 fig, ax = plt.subplots()
 
 # Import reference info
 ref = pd.read_csv(Path('ref/filters.csv'), index_col='name')
-
-y_max = 0
 
 for name, f in ref.iterrows():
     label = ' '.join([f.display_name, name])
@@ -40,7 +29,7 @@ for name, f in ref.iterrows():
     text_x = {'mean': np.mean(array.index), 
               'max': array.idxmax(), 
               'first': array.index[0] + 150}
-    text_y = {'above': array.max() + 0.7, 'below': -1, 'middle': array.max()/2}
+    text_y = {'above': array.max() + 0.8, 'below': -1.2, 'middle': array.max()/2}
     if f.instrument == 'GALEX':
         text_size = 10
         weight = 'bold'
@@ -54,35 +43,11 @@ for name, f in ref.iterrows():
     ax.plot(array.index, array.resp, label=label, ls=f.style, alpha=f.alpha, 
             c=f.color)
 
-    # Plot maxe
-    y_max = max(y_max, np.max(array.resp))
-
-x_ticks_major = np.arange(x_min, x_max + dx_major, dx_major)
-x_ticks_minor = np.arange(x_min, x_max + dx_minor, dx_minor)
-ax.xaxis.set_major_locator(ticker.MultipleLocator(dx_major))
-ax.xaxis.set_minor_locator(ticker.FixedLocator(x_ticks_minor))
-y_ticks_major = np.arange(y_min, y_max + dy_major, dy_major)
-y_ticks_minor = np.arange(y_min, y_max + dy_minor, dy_minor)
-ax.yaxis.set_major_locator(ticker.FixedLocator(y_ticks_major))
-ax.yaxis.set_minor_locator(ticker.FixedLocator(y_ticks_minor))
-ax.tick_params(axis='both', which='both', top=False, right=False)
-
-ax.spines['right'].set_visible(False)
-ax.spines['top'].set_visible(False)
-ax.spines['bottom'].set_bounds(x_min, x_max)
-ax.spines['left'].set_bounds(y_min, y_ticks_minor[-1])
-
-# Adjust ticks and frame
-ax.set_xlim((x_ticks_minor[0] - dx_minor, None))
-ax.set_ylim((-2.5 * dy_minor, None))
-
+ax.set_ylim((-3, None))
 ax.set_xlabel('Wavelength [Å]')
-# ax.set_xlim((1200, 3300))
-# ax.set_ylim((-1., None))
-ax.set_ylabel('Effective Throughput [%]', rotation='horizontal', ha='left', y=1.02, labelpad=0)
+ax.set_ylabel('Effective Throughput [%]')
 
-plt.tight_layout(pad=0.2)
-plt.subplots_adjust(top=0.9)
+plt.tight_layout(pad=0.3)
 
 plt.savefig(Path('out/filters.pdf'), bbox_inches='tight', dpi=300)
 plt.savefig(Path('out/filters.png'), bbox_inches='tight', dpi=300)
