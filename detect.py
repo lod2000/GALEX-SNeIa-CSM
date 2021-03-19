853e0ccb4bd65aa52f4161cd1c0909b0790cfc18
Liam Dubay <liam.dubay@gmail.com>
2021-03-19 12:20:03 -0700

    fixed detection code

--- detect.py
+++ detect.py
@@ -21,29 +21,29 @@ for sn_name in tqdm(sn_info.index):
         except (FileNotFoundError, pd.errors.EmptyDataError):
             continue
 
-    det = lc.detect_csm(SIGMA, SIGCOUNT)
+        det = lc.detect_csm(SIGMA, SIGCOUNT)
 
-    if len(det.index) > 0:
-        det_info = {
-            'name': sn_name, 
-            'band': band, 
-            'ndet': len(det.index),
-            'i0': det.index[0], 
-            'i1': det.index[-1], 
-            't0': det.loc[det.index[0],'t_delta_rest'],
-            't1': det.loc[det.index[-1], 't_delta_rest'],
-            'lmax': det['luminosity_hostsub'].max(),
-            'lmaxerr': det.loc[det['luminosity_hostsub'].idxmax(), 'luminosity_hostsub_err'],
-            'tmax': det.loc[det['luminosity_hostsub'].idxmax(), 't_delta_rest'],
-            'bg': lc.bg,
-            'bgerr': lc.bg_err,
-            'bgobs': lc.bg_data.shape[0]
-        }
-        detections.append(det_info)
-        # print(sn_name)
-        # print(band)
-        # print(det)
+        if len(det.index) > 0:
+            det_info = {
+                'name': sn_name, 
+                'band': band, 
+                'ndet': len(det.index),
+                'i0': det.index[0], 
+                'i1': det.index[-1], 
+                't0': det.loc[det.index[0],'t_delta_rest'],
+                't1': det.loc[det.index[-1], 't_delta_rest'],
+                'lmax': det['luminosity_hostsub'].max(),
+                'lmaxerr': det.loc[det['luminosity_hostsub'].idxmax(), 'luminosity_hostsub_err'],
+                'tmax': det.loc[det['luminosity_hostsub'].idxmax(), 't_delta_rest'],
+                'bg': lc.bg,
+                'bgerr': lc.bg_err,
+                'bgobs': lc.bg_data.shape[0]
+            }
+            detections.append(det_info)
+            # print(sn_name)
+            # print(band)
+            # print(det)
 
 detections = pd.DataFrame(detections)
 print(detections)
-# detections.to_csv('out/detections.csv')
+detections.to_csv('out/detections.csv')
--- light_curve.py
+++ light_curve.py
@@ -491,13 +491,15 @@ def plot_lc(ax, lc, tmax, all_points=False):
     # otherwise, just plot detections with limits for everything else
     if all_points:
         points = after
+        label = lc.band
     else:
         points = lc.detect_csm(DET_SIGMA, count=[1], dt_min=DT_MIN)
+        label = '%s det.' % lc.band
     if len(points.index) > 0:
         ax.errorbar(points[time_col], lambda_eff * points[data_col], 
                 yerr=lambda_eff * points[err_col], linestyle='none', ecolor=edge,
                 marker='o', ms=4, elinewidth=1, c=fill, mec=edge, mew=1,
-                label='%s det.' % lc.band
+                label=label
         )
 
     # Plot nondetection limits
