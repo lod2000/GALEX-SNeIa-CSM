baefda1fc998e774a566ea9cb149283d2cf978fd
Liam Dubay <liam.dubay@gmail.com>
2021-03-19 15:58:57 -0700

    added extra info printout

--- detect.py
+++ detect.py
@@ -11,6 +11,9 @@ SIGCOUNT = [1, 3]
 sn_info = pd.read_csv(Path('ref/sn_info.csv'), index_col='name')
 
 detections = []
+total_epochs = 0
+bg_epochs = 0
+post_epochs = 0
 
 for sn_name in tqdm(sn_info.index):
     sn = Supernova(sn_name, sn_info)
@@ -20,6 +23,10 @@ for sn_name in tqdm(sn_info.index):
             lc = LightCurve(sn, band)
         except (FileNotFoundError, pd.errors.EmptyDataError):
             continue
+
+        total_epochs += lc.data.shape[0]
+        bg_epochs += lc.bg_data.shape[0]
+        post_epochs += lc.data[lc.data['t_delta_rest'] > 0].shape[0]
 
         det = lc.detect_csm(SIGMA, SIGCOUNT)
 
@@ -40,10 +47,12 @@ for sn_name in tqdm(sn_info.index):
                 'bgobs': lc.bg_data.shape[0]
             }
             detections.append(det_info)
-            # print(sn_name)
-            # print(band)
-            # print(det)
 
 detections = pd.DataFrame(detections)
+print('\nDetections:')
 print(detections)
 detections.to_csv('out/detections.csv')
+
+print('\nTotal number of epochs: %s' % total_epochs)
+print('Total number of post-SN epochs: %s' % post_epochs)
+print('Avg pre-SN obs: %s' % (bg_epochs / len(sn_info.index)))
