ef6abea40e3554da5daec6edc09e05f04c832f5b
dubay.11 <dubay.11@ast-nc211568.asc.ohio-state.edu>
2021-09-09 15:28:58 -0400

    added Michael's gPhoton query scripts

---
+++ query_lightcurves.py
@@ -0,0 +1,128 @@
+import time
+from datetime import datetime
+import numpy as np
+import os, sys, shutil, glob
+import subprocess
+from gPhoton import gMap, gAperture as gAp
+import pandas as pd
+from astropy.coordinates import SkyCoord
+
+np.random.seed()
+Nproc = 10
+
+APRAD = 6.
+INRAD = 10.
+OUTRAD = 15.
+
+LCpath = 'historical_LCs'
+gAp_cmd='/home/dubay.11/anaconda3/bin/gAperture'
+
+INTERVAL = 60
+
+
+class Wrapper:
+	def __init__(self, name, ra, dec, band):
+		self.name = np.str.replace(name, ' ', '_')
+		self.ra = ra
+		self.dec = dec
+		self.band = band
+		if band == 'NUV':
+			self.csv = LCpath+'/'+self.name+'-NUV.csv'
+		elif band == 'FUV':
+			self.csv = LCpath+'/'+self.name+'-FUV.csv'
+		else:
+			raise ValueError('Unexpected band: %s' % band)
+	
+	def start(self):
+		if os.path.exists(self.csv):
+			self.done = True
+			return
+		else:
+			self.done = False
+		print('Starting query for: %s (band=%s) -> %s' % (self.name, self.band, self.csv))
+		cmd = make_cmd(ra=self.ra, dec=self.dec, csvff=self.csv, band=self.band)
+		self.starttime = datetime.utcnow()
+		self.process = subprocess.Popen(cmd)
+
+	def check(self):
+		status = self.process.poll()
+		if status is None: #still running
+			return
+		else:
+			self.done = True
+			dt = (datetime.utcnow() - self.starttime).total_seconds() / 3600.
+			print('\tquery finished %s (%.2f hrs)' % (self.csv, dt))
+
+
+def main(count=None):
+
+	catalog = pd.read_csv(Path('out/nearby_historical.csv'))
+	bands = catalog['band']
+	RA, DEC = convert_coords(catalog['ra'], catalog['dec'])
+
+	clean_empty_lcs()
+
+
+	jobs = []
+	for name, ra, dec, band in zip(catalog['sn_name'], RA, DEC, bands):
+		if band == 'both':
+			for b in ['NUV', 'FUV']:
+				job = Wrapper(name, ra, dec, b)
+				jobs.append(job)
+		else:
+			job = Wrapper(name, ra, dec, band)
+			jobs.append(job)
+
+	np.random.shuffle(jobs)
+
+	if count is not None:
+		jobs = [jobs[i] for i in range(count)]
+
+
+	running = []
+	while True:
+		running = check_running(running)
+		while len(running) < Nproc and len(jobs) > 0:
+			job = jobs.pop(0)
+			job.start()
+			if not job.done: running.append(job)
+
+		if len(jobs) == 0 and len(running) == 0: break
+		time.sleep(INTERVAL)
+
+def make_cmd(ra, dec, csvff, band):
+
+	aper = APRAD/3600.
+	ann1 = INRAD/3600.
+	ann2 = OUTRAD/3600.
+
+	cmd = [gAp_cmd, '--ra', str(ra), '--dec', str(dec), '--aperture', str(aper), '--inner', str(ann1), '--outer', str(ann2), '--csvfile', csvff, '--band', band]
+	return cmd
+
+def check_running(procs):
+	keep = []
+	for proc in procs:
+		proc.check()
+		if not proc.done: keep.append(proc)
+	return keep
+
+def clean_empty_lcs():
+	for ff in glob.glob('LCs/*.csv'):
+		Nlines = len([line.strip() for line in open(ff, 'r').readlines()])
+		if Nlines <= 1: os.remove(ff)
+
+def convert_coords(ralist, declist):
+	coords = SkyCoord(ralist, declist, unit='deg')
+	ra = coords.ra.deg
+	dec = coords.dec.deg
+	return ra, dec
+
+
+if __name__=='__main__':
+	import argparse
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--count', '-c', help='Number to run before exiting', default=None, type=int)
+
+	args = parser.parse_args()
+
+	main(count=args.count)
---
+++ quick_lc.py
@@ -0,0 +1,56 @@
+from query_lightcurves import Wrapper
+import pandas as pd
+import time, sys
+from astropy.coordinates import SkyCoord
+
+INTERVAL = 30 #sec b/w checks
+
+def main(name, band, ra=None, dec=None):
+
+    #parse coords
+    if ra is None and dec is None:
+        ra, dec = get_coords(name)
+    elif 'h' in ra and 'd' in ra:
+        coord = SkyCoord(ra, dec)
+        ra = coord.ra.deg
+        dec = coord.dec.deg
+    else:
+        ra = float(ra)
+        dec = float(dec)
+
+    #load job
+    if band == 'NUV':
+        job = Wrapper(name, ra, dec, 'NUV')
+    elif band == 'FUV':
+        job = Wrapper(name, ra, dec, 'FUV')
+    else:
+        print('Unrecognized band: %s' % band)
+        return
+    
+    #run query
+    job.start()
+    while True:
+        job.check()
+        if job.done: break
+        time.sleep(INTERVAL)
+    return
+
+def get_coords(name):
+    fname = None
+    if fname is None:
+        print('Need to setup name processor, exiting')
+        sys.exit()
+
+if __name__=='__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--name', '-n', help='name of SN', default=None, type=str)
+    parser.add_argument('--band', '-b', help='band to query, NUV or FUV', default=None, type=str)
+    parser.add_argument('--ra', '-r', help='ra in deg or HHhMMmSS.Ss, optional (can use name lookup)', default=None, type=str)
+    parser.add_argument('--dec', '-d', help='dec in deg or DDdMMmSS.Ss, optional', default=None, type=str)
+
+    args = parser.parse_args()
+    if args.name is None or args.band is None:
+        print('Need a SN name (--name) and UV band (--band)!')
+        sys.exit()
+    main(args.name, args.band, args.ra, args.dec)
