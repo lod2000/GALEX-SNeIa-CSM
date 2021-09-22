739bdffe20dba70a00a1c4027d3ac8558cc7ea9b
Liam Dubay <liam.dubay@gmail.com>
2021-09-22 17:36:26 -0400

    improved plots

--- calibrate_sed.py
+++ calibrate_sed.py
@@ -8,6 +8,7 @@ import astropy.units as u
 from astropy.visualization import quantity_support
 from tqdm import tqdm
 from matplotlib import pyplot as plt
+from matplotlib import lines as mlines
 from CSMmodel import CSMmodel
 
 # common unit macros
@@ -39,89 +40,123 @@ w_eff = {'FUV': 265.57 * u.angstrom,
 
 # flux with 0 color in AB system (flat in f_nu)
 zero_point_nu = 3.63e-20 * f_nu_units
-zero_point_lambda = lambda l: zero_point_nu * const.c.to('AA Hz') * l**-2
+
+# Plot color palette
+colors = {'FUV' : '#a37', 'NUV' : '#47a', 'F275W': '#e67'}
 
 # initialize CSM line emisison model
 line_model = CSMmodel(0, 250, 0.3, scale=1, model='Chev94')
 
-def main():
-    # import response curve, convert to effective area
-    response_table = np.genfromtxt(Path('ref/F275W.resp'), delimiter=' ')
-    wavelength = response_table[:,0] * u.angstrom
-    transmission = response_table[:,1]
-    hst_aper_rad = 120 * u.cm
-    effective_area = transmission * np.pi * hst_aper_rad**2
+def main(plot=True):
     
-    # calculate mean flux density of AB zero point as function of redshift
-    mfd0 = mean_flux_density(zero_point_lambda, transmission, wavelength) # at z=0
-    mfd_ab = lambda z: mfd0 * (1+z)**-3
-    print('mean flux density of AB zero point at z_15cp: ', f'{mfd_ab(SN2015cp["z"]):0.03e}')
+    # Convert HST luminosity of SN 2015cp to flux
+    SN2015cp['f_lambda'] = luminosity2flux(SN2015cp['L_lambda'], SN2015cp['z'], 
+                                           SN2015cp['dist'])
+    # ratio of AB zero point mean flux density at z_15cp to flux of 15cp
+    fratio_15cp = zero_point_mfd(SN2015cp['z'], 'F275W') / SN2015cp['f_lambda']
     
-    # mean flux density of line emission model at scale 1 (L_15cp)
-    # line_model = CSMmodel(0, 250, 0.3, scale=1, model='Chev94')
-    # line_lum = line_model(0, SN2015cp['z'])['F275W'] * L_lambda_units
-    # line_flux = luminosity2flux(line_lum, SN2015cp['z'], SN2015cp['dist'])
-    # print('flux density of line model at 15cp scale: ', f'{line_flux:0.03e}')
-    
-    # ratio between AB zero point and line model as function of redshift
-    fratio = lambda z: mfd_ab(z) / line_emission_flux(z)
-    print(fratio(SN2015cp['z']))
-    
-    # Convert HST luminosity to counts for SN 2015cp
-    SN2015cp['f_lambda'] = luminosity2flux(SN2015cp['L_lambda'], SN2015cp['z'], SN2015cp['dist'])
-    print('F275W flux of SN 2015cp: ', f"{SN2015cp['f_lambda']:0.03e}")
-    fratio_15cp = mfd_ab(SN2015cp['z']) / SN2015cp['f_lambda']
-    print(fratio_15cp)
-    mfd_ab_scaled = lambda z: mfd_ab(z) / fratio_15cp
-    
-    calibration = lambda z: mfd_ab_scaled(z) / line_emission_flux(z, dist=SN2015cp['dist'])
-    
-    # Plot fluxes
-    zarr = np.arange(0.01, 0.5, 0.01)
-    with quantity_support():
-        plt.plot(zarr, mfd_ab(zarr) / fratio_15cp, label='scaled AB zero point')
-        larr = np.array([line_model(0, z)['F275W'] for z in zarr]) * L_lambda_units
-        farr = luminosity2flux(larr, zarr, dist=SN2015cp['dist'])
-        plt.plot(zarr, farr, label='line-emission')
-        plt.axvline(SN2015cp['z'], 0, 1, c='r', label='SN 2015cp')
-        plt.yscale('log')
-        plt.xlabel('z')
-        plt.ylabel('mean flux density')
-        plt.suptitle('F275W')
-        plt.legend()
+    if plot:
+        # Plot mean flux density of AB zero point and line-emission model
+        fig, ax = plt.subplots(dpi=300)
+        dz = 0.01
+        zarr = np.arange(0., 0.5, dz)
+        with quantity_support():
+            for band in ['FUV', 'NUV', 'F275W']:
+                ax.plot(zarr, zero_point_mfd(zarr, band) / fratio_15cp, 
+                        color=colors[band], linestyle='--')
+                larr = np.array([line_model(0, z)[band] for z in zarr]) 
+                larr *= L_lambda_units
+                farr = luminosity2flux(larr, zarr, dist=SN2015cp['dist'])
+                ax.plot(zarr, farr, color=colors[band], label=band)
+        ax.set_xlabel('Redshift')
+        ax.set_ylabel(f'Mean flux density [{farr.unit:latex_inline}]')
+        ax.set_yscale('log')
+        # Add marker for SN 2015cp redshift
+        ax.axvline(SN2015cp['z'], 0, 1, c='gray', zorder=0)
+        ax.set_xticks(list(ax.get_xticks()[1:-1]) + [SN2015cp['z']])
+        ax.set_xticklabels(list(np.round(ax.get_xticks()[:-1], 1)) + ['15cp'])
+        fig.legend(loc='lower center', ncol=3, bbox_to_anchor=(0.5, 1.))
+        # Custom second legend
+        solid_line = mlines.Line2D([], [], color='k', linestyle='-')
+        dashed_line = mlines.Line2D([], [], color='k', linestyle='--')
+        fig.legend([solid_line, dashed_line], 
+                   ['Line-emission model', 'Scaled ZB zero point'],
+                   loc='lower center', ncol=2, bbox_to_anchor=(0.5, 0.9))
         plt.show()
-        
-    # Plot calibrations
-    plt.plot(zarr, [calibration(z) for z in zarr])
-    plt.axvline(SN2015cp['z'], 0, 1, c='r', label='SN 2015cp')
-    plt.yscale('log')
-    plt.xlabel('z')
-    plt.ylabel('calibration')
-    plt.suptitle('F275W calibration')
-    plt.show()
     
-    # fratio_15cp = SN2015cp['f_lambda'] / (mfd_flat / (1+SN2015cp['z'])**3)
-    # fratio = lambda z: SN2015cp['f_lambda'] / (mfd_flat / (1+z)**3)
-    # fratio_15cp = fratio(SN2015cp['z'])
-    # photon_energy = (const.h * const.c / lambda_eff['F275W']).to('erg')
-    # electron_flux = SN2015cp['f_lambda'] / photon_energy * u.count # ct/s/cm2/AA
-    # wstep = wavelength[1:] - wavelength[:-1]
-    # wstep = np.append(wstep, wstep[-1])
-    # SN2015cp['counts'] = electron_flux * np.sum(wstep * effective_area)
-    # print(SN2015cp['counts'])
-    # print(SN2015cp['f_lambda'] / SN2015cp['counts'])
-    
-    # Calculate line model flux in F275W band at scale 1
-    # print(SN2015cp['f_lambda'] / line_flux)
-    # print(fratio_15cp * mfd_flat / line_flux)
-    
-    test_z = 0.1
-    # test_lum = line_model(0, test_z)['F275W'] * L_lambda_units
-    # test_flux = luminosity2flux(test_lum, test_z)
-    # print(fratio(test_z) * (mfd_flat / (1+test_z)**3) / test_flux)
+        # Plot calibration of each band
+        fig, ax = plt.subplots(dpi=300)
+        for band in ['FUV', 'NUV', 'F275W']:
+            ax.plot(zarr, gen_calibration(zarr, band), 
+                    label=band, color=colors[band])
+        ax.set_xlabel('Redshift')
+        ax.set_ylabel('Calibration')
+        ax.set_yscale('log')
+        # Add horizontal marker at unity
+        ax.axhline(1, 0, 1, c='gray', zorder=0, linestyle=':')
+        # Add marker for SN 2015cp redshift
+        ax.axvline(SN2015cp['z'], 0, 1, c='gray', zorder=0)
+        ax.set_xticks(list(ax.get_xticks()[1:-1]) + [SN2015cp['z']])
+        ax.set_xticklabels(list(np.round(ax.get_xticks()[:-1], 1)) + ['15cp'])
+        fig.legend(loc='lower center', ncol=3, bbox_to_anchor=(0.5, 0.9))
+        plt.show()
+
+
+def gen_calibration(z, band):
+    """
+    Ratio of the line-emission flux density to the scaled AB zero point flux.
+
+    Parameters
+    ----------
+    z : float
+        Redshift of source, which scales the flux by (1+z)^-3.
+    band : str
+        Filter, one of 'FUV', 'NUV', or 'F275W'.
+
+    Returns
+    -------
+    calibration : float
+        Scaled flux ratio.
+
+    """
+    # ratio of AB zero point mean flux density at z_15cp to flux of 15cp
+    fratio_15cp = zero_point_mfd(SN2015cp['z'], 'F275W') / SN2015cp['f_lambda']
+    calibration = line_emission_flux(z, dist=SN2015cp['dist'], band=band)
+    calibration /= zero_point_mfd(z, band) / fratio_15cp
+    return calibration.value
+
+
+def zero_point_mfd(z, band):
+    """
+    Calculate mean flux density of the AB zero point flux.
+
+    Parameters
+    ----------
+    z : float
+        Redshift of source, which scales the flux by (1+z)^-3.
+    band : str
+        Filter, one of 'FUV', 'NUV', or 'F275W'.
+
+    Returns
+    -------
+    mfd : astropy.units.quantity.Quantity
+        Mean flux density per unit wavelength of the AB zero point.
+
+    """    
+    wavelength, transmission, effective_area = import_response_table(band)
+    # convert AB zero point flux from frequency space to wavelength space
+    zero_point_lambda = lambda l: zero_point_nu * const.c.to('AA Hz') * l**-2
+    # calculate mean flux density of the AB zero point in the given band
+    mfd0 = mean_flux_density(zero_point_lambda, transmission, wavelength) 
+    # scale mean flux density by redshift factor
+    mfd = mfd0 * (1+z)**-3
+    return mfd
+
 
 def mean_flux_density(source_flux, throughput, wavelength):
     """
+    Integrate a source flux distribution over a bandpass filter.
+    
     Calculate the mean flux density to be observed given an assumed
     intrinsic source flux distribution and passband throughput over a range
     of wavelengths.
@@ -130,18 +165,17 @@ def mean_flux_density(source_flux, throughput, wavelength):
     ----------
     source_flux : function 
         Source flux distribution as a function of wavelength.
-    throughput : ndarray
+    throughput : numpy.ndarray
         Throughput of passband filter.
-    wavelength : Quantity or ndarray 
+    wavelength : astropy.units.quantity.Quantity or numpy.ndarray 
         Wavelengths corresponding to throughput. Array must be same length as 
         throughput. If provided unitless, assumed to be angstroms.
     
     Returns
     -------
-    mfd : Quantity
-        Observed mean flux density per unit wavelength
+    mfd : astropy.units.quantity.Quantity
+        Observed mean flux density per unit wavelength.
     """
-    
     # Calculate step between successive wavelengths
     if type(wavelength) == np.ndarray:
         wavelength *= u.angstrom
@@ -151,16 +185,17 @@ def mean_flux_density(source_flux, throughput, wavelength):
     mfd /= np.sum(throughput * wavelength * wstep)
     return mfd
 
+
 def line_emission_flux(z, dist=0., band='F275W'):
     """
     Calculate flux from CSM line-emission model in a given redshift and filter.
 
     Parameters
     ----------
-    z : float
+    z : float or numpy.ndarray
         Redshift of source, which applies a horizontal shift to the spectrum
         as well as an overall decrease in flux.
-    dist : Quantity or float, optional
+    dist : astropy.units.quantity.Quantity or float, optional
         Distance to source, which is assumed to be in Mpc if provided as a
         float. If 0, the distance is automatically calculated from the redshift
         by the Hubble relation. The default is 0.
@@ -169,18 +204,21 @@ def line_emission_flux(z, dist=0., band='F275W'):
 
     Returns
     -------
-    line_flux : Quantity
+    line_flux : astropy.units.quantity.Quantity
         Spectral flux density per unit wavelength.
     """
-    
-    line_lum = line_model(0, z)[band] * L_lambda_units
+    if type(z) == np.ndarray:
+        line_lum = np.array([line_model(0, val)[band] for val in np.nditer(z)])
+        line_lum *= L_lambda_units
+    else:
+        line_lum = line_model(0, z)[band] * L_lambda_units
     line_flux = luminosity2flux(line_lum, z, dist=dist)
-    return line_flux   
+    return line_flux
+
 
 def luminosity2flux(luminosity, z, dist=0., h0=H0):
     """
-    Convert intrinsic luminosity to observed flux at a given redshift and 
-    distance, assuming a flat cosmology.
+    Convert intrinsic luminosity to observed flux assuming a flat cosmology.
     
     Parameters
     ----------
@@ -188,25 +226,67 @@ def luminosity2flux(luminosity, z, dist=0., h0=H0):
         Spectral luminosity density per unit wavelength.
     z : float
         Redshift of source.
-    dist : Quantity or float, optional
+    dist : astropy.units.quantity.Quantity or float, optional
         Distance to source, which is assumed to be in Mpc if provided as a
         float. If 0, the distance is automatically calculated from the redshift
         by the Hubble relation. The default is 0.
-    H0 : Quantity
+    H0 : astropy.units.quantity.Quantity
         The Hubble constant to be used to calculate distance.
         
     Returns
     -------
-    flux : Quantity
+    flux : astropy.units.quantity.Quantity
         Spectral flux density per unit wavelength.
     """
-
     if dist == 0.:
         dist = const.c.to('km/s') * z / h0
     if type(dist) == float:
         dist *= u.Mpc
     flux = luminosity / (4 * np.pi * dist.to('cm')**2 * (1+z)**3)
-    return flux 
+    return flux
+
+
+def import_response_table(band):
+    """
+    Import SVO filter response table.
+
+    Parameters
+    ----------
+    band : str
+        Filter, one of 'FUV', 'NUV', or 'F275W'.
+
+    Raises
+    ------
+    ValueError
+        Value for 'band' outside acceptable list.
+
+    Returns
+    -------
+    wavelength : astropy.units.quantity.Quantity
+        Array of wavelengths in response table.
+    transmission : numpy.ndarray
+        Filter transmission at each wavelength.
+    effective_area : astropy.units.quantity.Quantity
+        Effective area (transmission times objective area) at each wavelength.
+
+    """
+    if band not in ['FUV', 'NUV', 'F275W']:
+        raise ValueError('Band must be FUV, NUV, or F275W')
+    
+    response_table = np.genfromtxt(Path('ref/%s.resp' % band), delimiter=' ')
+    wavelength = response_table[:,0] * u.angstrom
+    # HST and GALEX responses are given in different units
+    if band == 'F275W':
+        transmission = response_table[:,1]
+        hst_aper_rad = 120 * u.cm
+        effective_area = transmission * np.pi * hst_aper_rad**2
+    else:
+        effective_area = response_table[:,1] * u.cm**2
+        galex_aper_rad = 25 * u.cm
+        transmission = effective_area / (np.pi * galex_aper_rad**2)
+    
+    return wavelength, transmission, effective_area
+
 
 if __name__ == '__main__':
     main()
